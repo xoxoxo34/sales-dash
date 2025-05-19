b24d32df5d506f69313d2803df4a19ad1f19e529
Lungile Princess Phuti <99497273+xoxoxo34@users.noreply.github.com>
2025-05-19 08:18:39 +0200

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -15,7 +15,12 @@ packages = [
 # Function to install packages
 def install_packages(packages):
     for package in packages:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+        try:
+            print(f"Installing {package}...")
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+            print(f"{package} installed successfully.\n")
+        except subprocess.CalledProcessError:
+            print(f"Failed to install {package}. Please check your internet connection or pip setup.\n")
 
 if __name__ == "__main__":
     install_packages(packages)
